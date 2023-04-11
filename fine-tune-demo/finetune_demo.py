ee31f13cd6dedf6980bba7d563de4c21fba04de1
nanachi1027 <nanachi1027@163.com>
2023-04-11 14:37:30 +0800

    upload first python openai api demo

--- fine-tune-demo/finetune_demo.py
+++ fine-tune-demo/finetune_demo.py
@@ -24,3 +24,37 @@ prompt = "Samsung Galaxy Feel"
 response = openai.Completion.create(model=model,prompt=prompt)
 print(response)
 print(response["choices"][0]["text"])
+
+import json
+
+# set up the OpenAI API key
+openai.api_key = "sk-xxxx"
+
+# set up the prompt
+prompt = "What is the meaning of life?"
+
+# generate text using the OpenAI API
+response = openai.Completion.create(
+  engine="davinci", # chose your prefer engine here 
+  prompt=prompt,
+  max_tokens=100,
+  n=1,
+  stop=None,
+  temperature=0.7
+)
+
+# print the generated text
+print(response.choices[0].text)
+
+# provide feedback on the generated text
+feedback = {
+  "text": response.choices[0].text,
+  "model": "davinci",
+  "prompt": prompt,
+  "rating": 4, # rating value 
+  "comment": "This is a great response!"
+}
+
+# send the feedback to the OpenAI API to enhance model 
+response = openai.Feedback.create(json.dumps(feedback))
+
